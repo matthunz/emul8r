3d88e59b07905e4de032c5efc5f09c1287c2a9aa
Matt Hunzinger <mhunzing2@gmail.com>
2019-01-09 22:20:17 -0500

    First opcode: JP(addr)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+enum Op {
+    JP(u16),
+}
+
+struct Cpu {
+    pc: u16,
+}
+
+impl Cpu {
+    fn new() -> Self {
+        Self { pc: 0 }
+    }
+    fn opcode(&mut self, op: Op) {
+        match op {
+            Op::JP(addr) => self.pc = addr,
+        }
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn cpu() {
+        let mut cpu = Cpu::new();
+        cpu.opcode(Op::JP(4));
+        assert_eq!(cpu.pc, 4);
+    }
 }
